f2ad3e66117bec43fd6d121485a007e2a8c0f384
Christopher Rugg <christopherrugg@Christophers-MacBook-Pro.local>
2017-07-10 16:03:06 -0400

    emit and listen events

--- server/server.js
+++ server/server.js
@@ -14,6 +14,27 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('new user connected');
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
+
+  socket.on('createMessage', (msg) => {
+    console.log('createMessage', msg);
+    io.emit('newMessage', {
+      from: msg.from,
+      text: msg.text,
+      createdAt: new Date().getTime()
+    });
+  });
+
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
